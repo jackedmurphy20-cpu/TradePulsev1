869beb2c600441483c2f605210ea9344fe8c8637
Jack Murphy <jackedmurphy20@gmail.com>
2026-03-19 21:14:02 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
-import MobileNav from './MobileNav';
+import MobileNav from './MobileNav.jsx';
 
 export default function AppLayout() {
   return (
